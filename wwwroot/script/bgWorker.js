a3f9c77240781615c456d355f1473fab9d9ce531
Mathias Andersson (Stratiteq) <mandersson@stratiteq.com>
2024-02-07 15:16:17 +0100

    Not needed for repro but added some basic code in the web worker

--- wwwroot/script/bgWorker.js
+++ wwwroot/script/bgWorker.js
@@ -1,1 +1,12 @@
-﻿
+﻿// This is just an empty worker....
+class ClientBackgroundWorker {
+    constructor() {
+        this._enableDebugLog = false;        
+    }    
+}
+
+let worker = new ClientBackgroundWorker(indexedDB);
+console.log("** --- Background worker created --- ***");
+self.onmessage = (e) => {
+    console.log(`** --- Background worker message: ${e.data}`);        
+};
